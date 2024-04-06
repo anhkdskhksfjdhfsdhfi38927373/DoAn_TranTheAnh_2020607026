9aa7b14d000b0c39cd32e670edd1f238e77e39ae
trant <trant@DESKTOP-6PGOPNS>
2024-04-06 15:45:39 +0700

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Doan_TranTheAnh_2020607026.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Doan_TranTheAnh_2020607026.Models
+{
+    public class Product
+    {
+        [Key]
+        public string ProductID { get; set; }
+        [Required]
+        public string? ProductName { get; set; }
+        [Required]
+        public string ProductImage { get; set; }
+        [Required]
+        public decimal? Price { get; set; }
+            [Required]
+        public string? Color { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public string Size { get; set; }
+
+    }
+}
